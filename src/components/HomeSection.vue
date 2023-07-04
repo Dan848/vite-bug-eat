752231ca3a2f295a580ab7f64da31859690b0d6f
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-07-04 17:54:21 +0200

    add variable color

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-
+  <div class="container-fluid">
+    <div class="container"></div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../assets/partials/var.scss" as *;
+.container-fluid {
+  background-color: $primary;
+}
+</style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-
-  </div>
+  <section>
+    <HomeView />
+  </section>
 </template>
 
 <script>
-
+import HomeSection from "../components/HomeSection.vue";
 export default {
-  name: 'HomeView'
-}
+  name: "HomeView",
+};
 </script>
 
 <style lang="scss" scoped></style>
