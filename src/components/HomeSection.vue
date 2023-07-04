7ae7930e27417e230513f2340c81f1c78fd24b4d
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-07-04 18:10:04 +0200

    first jumbotron

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -1,16 +1,7 @@
-<template>
-  <div class="container-fluid">
-    <div class="container"></div>
-  </div>
-</template>
+<template></template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped>
-@use "../assets/partials/var.scss" as *;
-.container-fluid {
-  background-color: $primary;
-}
-</style>
+<style lang="scss" scoped></style>
