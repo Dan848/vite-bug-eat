3094a129b07337455b0f9f43571f41d7653e03de
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-07-05 15:52:44 +0200

    add apiUrl

--- src/data/store.js
+++ src/data/store.js
@@ -1,17 +1,9 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-<<<<<<< HEAD
   imageBaseUrl: "http://localhost:8000",
   apiUrl: "http://127.0.0.1:8000/api",
   types: [],
-=======
-  imgStartUrl: 'http://localhost:8000',
-  apiURL: 'http://127.0.0.1:8000/api',
-  restaurants: [],
-  currentPage: 1,
-  lastPage: null,
->>>>>>> create-card-restaurant
   headerLinks: [
     {
       label: "Home",
