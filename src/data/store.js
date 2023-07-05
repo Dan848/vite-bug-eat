25991e71526f034ac0b1ff8184151c0523183de9
NelloRossi <rossianiello97@gmail.com>
2023-07-05 13:00:44 +0200

    added restaurant card and list

--- src/data/store.js
+++ src/data/store.js
@@ -1,6 +1,11 @@
 import { reactive } from "vue";
 
 export const store = reactive({
+  imgStartUrl: 'http://localhost:8000',
+  apiURL: 'http://127.0.0.1:8000/api',
+  restaurants: [],
+  currentPage: 1,
+  lastPage: null,
   headerLinks: [
     {
       label: "Home",
