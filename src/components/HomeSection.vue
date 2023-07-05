a7cf7ea2a4dfcde35b222cdf3779bdcf6ce0f290
peppeluc22 <gluca0260@gmail.com>
2023-07-05 18:49:05 +0200

    created homepage section

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -1,15 +1,73 @@
-<template></template>
+<template>
+  <section class="container-fluid d-flex" :class="section.inverted ? 'bg-inverted py-5' : 'bm-py-5'">
+    <div class="container w-75">
+      <div class="align-items-center flex-wrap" :class="section.inverted ? 'd-flex flex-row-reverse' : 'row'">
+        <!-- Content Col N.1 -->
+        <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center">
+          <!-- Div with title and subtitle -->
+          <div class="section-content">
+            <div>
+              <h2>{{ section.title }}</h2>
+              <p v-if="section.subtitle" class="subtitle">{{ section.subtitle }}</p>
+            </div>
+
+            <p class="lh-lg">
+              {{ section.content }}
+            </p>
+          </div>
+
+          <div class="d-flex justify-content-center align-items-center">
+            <router-link v-if="Object.keys(section.routerLink).length" :to="{ name: section.routerLink.href }"
+              class="text-decoration-none bm-btn">
+              <button class="bm-btn">
+                {{ section.routerLink.buttonText }}
+              </button>
+            </router-link>
+          </div>
+
+        </div>
+
+        <!-- Image Col -->
+        <div class="col-12 col-md-6 sectionImg-box" :class="section.inverted ? 'pe-5' : 'ps-5'">
+          <img :src="section.image" :alt="section.title" class="img-fluid w-100">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
 
 <script>
-export default {};
+export default {
+  name: 'HomeSection',
+  props: {
+    'section': Object
+  }
+
+};
 </script>
 
 
 <style lang="scss" scoped>
 @use "../assets/partials/variable.scss" as *;
 
-.container-fluid {
+.bg-inverted {
   background-color: $primary;
+  color: white;
+
+  .section-content:hover {
+    color: $secondary;
+  }
+}
+
+.section-content:hover {
+  transform: scale(1.1);
+  color: $primary;
+  transition: 1s;
+  cursor: pointer;
+}
+
+.bm-py-5 {
+  padding: 5rem 0;
 }
 </style>
 
