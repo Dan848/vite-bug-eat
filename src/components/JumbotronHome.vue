3432be9ed43e63a87011844c89699cef19f1c49f
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-07-07 11:08:18 +0200

    modified by daniele order's

--- src/components/JumbotronHome.vue
+++ src/components/JumbotronHome.vue
@@ -1,28 +1,34 @@
 <template>
-    <div class="jumbotron d-flex justify-content-center py-5">
-        <div class="d-flex flex-column justify-content-center align-items-center">
-            <img src="public/img/bees.gif" class="jumbo-logo" alt="" />
-            <div class="pt-5">
-                <h2> Qualcosa ronza in giro... </h2>
-                <p> ...sono i nostri rider! </p>
-            </div>
-        </div>
+  <div class="jumbotron d-flex justify-content-center py-5">
+    <div class="d-flex flex-column justify-content-center align-items-center">
+      <img src="public/img/bees.gif" class="jumbo-logo" alt="" />
+      <div class="pt-5">
+        <h2>Qualcosa ronza in giro...</h2>
+        <p>...sono i nostri rider!</p>
+      </div>
     </div>
+  </div>
 
-    <div class="row height d-flex justify-content-center align-items-center">
-        <div class="col-md-6">
-            <div class="form">
-                <i class="fa fa-search"></i>
-                <input type="text" class="form-control form-input" placeholder="Cerca il ristorante più vicino a te..." />
-            </div>
-        </div>
+  <div class="row height d-flex justify-content-center align-items-center">
+    <div class="col-md-6">
+      <div class="form">
+        <i class="fa fa-search"></i>
+        <input
+          type="text"
+          class="form-control form-input"
+          placeholder="Cerca il ristorante più vicino a te..."
+        />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'JumbotronHome'
-}
+  name: "JumbotronHome",
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../assets/partials/variable.scss" as *;
+</style>
