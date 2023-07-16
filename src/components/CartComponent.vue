617eaa3bb8917ca9f5298370d7b5ec4976142d4f
Dan848 <daniele.ceravolo15@gmail.com>
2023-07-16 14:08:10 +0200

    fixed send orders

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -309,17 +309,6 @@ export default {
       const cart = store.cart;
       localStorage.setItem("cart", JSON.stringify(cart));
     },
-    //Format data
-    formatDateTime(dateTime) {
-      const year = dateTime.getFullYear();
-      const month = (dateTime.getMonth() + 1).toString().padStart(2, "0");
-      const day = dateTime.getDate().toString().padStart(2, "0");
-      const hours = dateTime.getHours().toString().padStart(2, "0");
-      const minutes = dateTime.getMinutes().toString().padStart(2, "0");
-      const seconds = dateTime.getSeconds().toString().padStart(2, "0");
-
-      return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    },
     //Change Input when Filled
     handleFocus(id) {
       const element = document.getElementById(id);
--- src/pages/CheckoutView.vue
+++ src/pages/CheckoutView.vue
@@ -72,6 +72,17 @@ export default {
     };
   },
   methods: {
+    //Format data
+    formatDateTime(dateTime) {
+      const year = dateTime.getFullYear();
+      const month = (dateTime.getMonth() + 1).toString().padStart(2, "0");
+      const day = dateTime.getDate().toString().padStart(2, "0");
+      const hours = dateTime.getHours().toString().padStart(2, "0");
+      const minutes = dateTime.getMinutes().toString().padStart(2, "0");
+      const seconds = dateTime.getSeconds().toString().padStart(2, "0");
+
+      return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    },
     //Send form data
     sendForm() {
       if (store.cart.products.length >= 1) {
